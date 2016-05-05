38a309a8536486b412b52f7fc813aa4b549e101f
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:43:19 -0700

    Commit again

--- Controlling the Keyboard/ViewController.swift
+++ Controlling the Keyboard/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func button(sender: AnyObject) {
+        label.text = textField.text
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
 
